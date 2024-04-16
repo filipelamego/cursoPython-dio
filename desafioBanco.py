8048bca3e038d1aa634f9b8b8ebeaa55a3950c2d
filipelamego <filipeslamego@gmail.com>
2024-04-16 03:17:01 -0300

    Subindo código python no repositório do curso DIO

---
+++ desafioBanco.py
@@ -0,0 +1,67 @@
+menu = """
+
+[d] Depositar
+[s] Sacar
+[e] Extrato
+[q] Sair
+
+=> """
+
+saldo = 0
+limite = 500
+extrato = ""
+numero_saques = 0
+LIMITE_SAQUES = 3
+
+while True:
+    opcao = input(menu)
+
+    if opcao == "d":
+        valor = float(input("Informe o valor do depósito: "))
+
+        if valor > 0:
+            saldo += valor
+            extrato += f"Depósito: R$ {valor:.2f}\n"
+
+        else:
+            print("Operação falhou! O valor informado não é válido.")
+
+
+    elif opcao == "s":
+        valor = float(input("Informe o valor do saque: "))
+
+        excedeu_saldo = valor > saldo
+
+        excedeu_limite = valor > limite
+
+        excedeu_saques = numero_saques >= LIMITE_SAQUES
+
+        if excedeu_saldo:
+            print("Operação inválida! Seu saldo é insuficiente.")
+
+        elif excedeu_limite:
+            print("Operação inválida! O valor do saque excede seu limite diário.")
+
+        elif excedeu_saques:
+            print("Operação inválida! Número máximo de saques excedido.")
+
+        elif valor > 0:
+            saldo -= valor
+            extrato += f"Saque: R${valor:.2f}\n"
+            numero_saques += 1
+
+        else:
+            print("Operação inválida! Valor informado é inválido.")
+
+
+    elif opcao == "e":
+        print("\n=========== EXTRATO ===========")
+        print("Nâo há movimentações registradas" if not extrato else extrato)
+        print(f"\nSaldo: R${saldo:.2f}")
+        print("=================================")
+
+    elif opcao == "q":
+        break
+
+    else: 
+        print("Opção inválida! Escolha uma opção válida do menu")
